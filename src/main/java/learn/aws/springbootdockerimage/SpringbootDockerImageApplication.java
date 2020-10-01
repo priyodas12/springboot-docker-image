414a6ba1c898ae36fe940cd8f4fbd80c0f5cb9a4
Priyobrato Das <priyodas12@gmail.com>
2020-10-01 11:59:39 +0530

    Initial commit

---
+++ src/main/java/learn/aws/springbootdockerimage/SpringbootDockerImageApplication.java
@@ -0,0 +1,13 @@
+package learn.aws.springbootdockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerImageApplication.class, args);
+	}
+
+}
---
+++ src/main/java/learn/aws/springbootdockerimage/controller/GenericController.java
@@ -0,0 +1,17 @@
+package learn.aws.springbootdockerimage.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/docker")
+public class GenericController {
+
+    @GetMapping("/image")
+    public String displayRandomDockerImage(){
+      return  "creating docker image :"+ UUID.randomUUID();
+    }
+}
